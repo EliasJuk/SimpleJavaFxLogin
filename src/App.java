bd87ee697b436c439fd74f3d0fa2f0e589d874ba
eliasjuk <eliasp.juk@hotmail.com>
2024-10-12 21:57:21 -0300

    Carregar a tela de login

--- src/App.java
+++ src/App.java
@@ -1,5 +1,22 @@
-public class App {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application{
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("scenes/sceneLogin.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene login = new Scene(root);
+
+        primaryStage.setTitle("Login");
+        primaryStage.setScene(login);
+        primaryStage.show();
     }
 }
